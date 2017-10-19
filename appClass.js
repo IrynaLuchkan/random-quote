e83cc1fbd599892f57841f3c097f5949b1eb3a55
IrynaLuchkan <lirynaa@gmail.com>
2017-10-19 15:28:04 +0300

    class editted, functions are divided

--- appClass.js
+++ appClass.js
@@ -9,69 +9,68 @@ window.onload = function () {
         }
 
         addClickEvent() {                       
-            this.btnGenerate.addEventListener("click", () => {
-                
-                this.btnGenerate.innerHTML = "Please wait";
-                return fetch(this.myQuoteUrl, {
-                    method: 'GET',
-                    type: 'cors',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin':'*',
-                        'pragma': 'no-cache',
-                        'cache-control': 'no-cache'
-                    },
-                })
-                .then(res => 
-                    {
-                        if(res.headers.get("content-type") &&
-                            res.headers.get("content-type").toLowerCase().indexOf("application/json") !== -1) {                      
-                            return res.json()
-                        } else {
-                        throw new TypeError()
-                    }
-                })
-                .then((processJSON) => {           
-                    this.quoteText.innerHTML = processJSON[0].content;
-                    this.quoteAutor.innerHTML = processJSON[0].title;
-                    this.btnGenerate.innerHTML = "Get new quote";
-
-                    //creating tweeter button
-                    document.getElementById('tweetContainer').innerHTML = '';
-
-                    twttr.widgets.createShareButton(
-                        '',
-                        document.getElementById('tweetContainer'),
-                        {
-                            text: `${this.quoteText.textContent} -${this.quoteAutor.textContent}`
-                        }
-                    );
-                })
-                .catch((err) => {
-                    console.log(err);
-                    this.quoteText.innerHTML = "Error occurs. Please check if CORS enabled.";
-                })            
-            })
+            this.btnGenerate.addEventListener('click', () => this._parseQuoteInfo());
         }
+
+        _getQuote() {
+            let qouteRequestResult = new Promise(result => {
+                let xmlhttp = new XMLHttpRequest();                
+                xmlhttp.open('GET', this.myQuoteUrl, true);
+                xmlhttp.setRequestHeader('Cache-Control', 'max-age=0');
+                xmlhttp.send();
+                xmlhttp.onreadystatechange = () => {
+                    if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
+                    {                 
+                        result(JSON.parse(xmlhttp.responseText));
+                    } else {this.quoteText.innerHTML = "Error occurs. Please check if CORS enabled.";};
+                };
+            }); 
+            return qouteRequestResult;
+        }
+
+        _parseQuoteInfo() {  
+            this.btnGenerate.innerHTML = 'Please wait';          
+            this._getQuote().then((qouteJSON) => {           
+                this.quoteText.innerHTML = qouteJSON[0].content;
+                this.quoteAutor.innerHTML = qouteJSON[0].title;
+
+                this.btnGenerate.innerHTML = 'Get new quote';
+
+                //creating tweeter button
+                // document.getElementById('tweetContainer').innerHTML = '';
+
+                // twttr.widgets.createShareButton(
+                //     '',
+                //     document.getElementById('tweetContainer'),
+                //     {
+                //         text: `${this.quoteText.textContent} -${this.quoteAutor.textContent}`
+                //     }
+                // )
+            })
+            .catch((err) => {
+                console.log(err);
+                this.quoteText.innerHTML = 'Error occurs. Please check if quote containers are correct.';
+            })
+        };      
     }
+
 // tweeter function
-    window.twttr = (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
+    // window.twttr = (function(d, s, id) {
+    //     var js, fjs = d.getElementsByTagName(s)[0],
+    //         t = window.twttr || {};
+    //     if (d.getElementById(id)) return t;
+    //     js = d.createElement(s);
+    //     js.id = id;
+    //     js.src = "https://platform.twitter.com/widgets.js";
+    //     fjs.parentNode.insertBefore(js, fjs);
 
-        t._e = [];
-        t.ready = function(f) {
-            t._e.push(f);
-        };
+    //     t._e = [];
+    //     t.ready = function(f) {
+    //         t._e.push(f);
+    //     };
 
-        return t;
-    }(document, "script", "twitter-wjs"));
+    //     return t;
+    // }(document, "script", "twitter-wjs"));
 
     let generateQuote = new GenerateQuote('#generate-quote', '#quote-text', '#quote-autor');
     
