fdd943fe4de02747b90de17cb103d5aa7cfe053c
IrynaLuchkan <lirynaa@gmail.com>
2017-10-19 10:51:55 +0300

    class edited

--- appClass.js
+++ appClass.js
@@ -8,66 +8,66 @@ window.onload = function () {
         }
 
         addClickEvent() {
-            console.log('hi');
-            this.btnGenerate.addEventListener("click", this.downloadNow);
-        };        
-        
-        downloadNow() {
-            return fetch(this.myQuoteUrl, {
-                method: 'GET',
-                type: 'cors',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin':'*',
-                    'pragma': 'no-cache',
-                    'cache-control': 'no-cache'
-                },
-            })
-            .then(res => 
-                {
-                    if(res.headers.get("content-type") &&
-                        res.headers.get("content-type").toLowerCase().indexOf("application/json") !== -1) {                      
-                        return res.json()
-                    } else {
-                    throw new TypeError()
-                }
-            })
-            .then(function(processJSON){           
-                elemQuoteText.innerHTML = processJSON[0].content;
-                document.getElementById('quote-autor').innerHTML = processJSON[0].title;
-                document.getElementById('tweetContainer').innerHTML = '';
-
-                twttr.widgets.createShareButton(
-                    '',
-                    document.getElementById('tweetContainer'),
+            console.log('hi');            
+            this.btnGenerate.addEventListener("click", () => {
+                console.log(this);
+                return fetch(this.myQuoteUrl, {
+                    method: 'GET',
+                    type: 'cors',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin':'*',
+                        'pragma': 'no-cache',
+                        'cache-control': 'no-cache'
+                    },
+                })
+                .then(res => 
                     {
-                        text: `${elemQuoteText.textContent} -${document.getElementById('quote-autor').textContent}`
+                        if(res.headers.get("content-type") &&
+                            res.headers.get("content-type").toLowerCase().indexOf("application/json") !== -1) {                      
+                            return res.json()
+                        } else {
+                        throw new TypeError()
                     }
-                );
-            })
-            .catch(function () {
-                document.getElementById('quote-text').innerHTML = "OH NOOOOOOOOOO";
-            })  
+                })
+                .then(function(processJSON){           
+                    elemQuoteText.innerHTML = processJSON[0].content;
+                    document.getElementById('quote-autor').innerHTML = processJSON[0].title;
+                    // document.getElementById('tweetContainer').innerHTML = '';
+
+                    // twttr.widgets.createShareButton(
+                    //     '',
+                    //     document.getElementById('tweetContainer'),
+                    //     {
+                    //         text: `${elemQuoteText.textContent} -${document.getElementById('quote-autor').textContent}`
+                    //     }
+                    // );
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    document.getElementById('quote-text').innerHTML = "OH NOOOOOOOOOO";
+                })            
+            });
         }
     }
 
-    window.twttr = (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
+    // window.twttr = (function(d, s, id) {
+    //     var js, fjs = d.getElementsByTagName(s)[0],
+    //         t = window.twttr || {};
+    //     if (d.getElementById(id)) return t;
+    //     js = d.createElement(s);
+    //     js.id = id;
+    //     js.src = "https://platform.twitter.com/widgets.js";
+    //     fjs.parentNode.insertBefore(js, fjs);
 
-        t._e = [];
-        t.ready = function(f) {
-            t._e.push(f);
-        };
+    //     t._e = [];
+    //     t.ready = function(f) {
+    //         t._e.push(f);
+    //     };
 
-        return t;
-    }(document, "script", "twitter-wjs"));
+    //     return t;
+    // }(document, "script", "twitter-wjs"));
 
     console.log('hi again');
 
@@ -79,7 +79,7 @@ window.onload = function () {
 
 
         //var btnGenerate = document.getElementById("generate-quote"),
-        let btnTwit = document.getElementById("tweetMessage");
+    // let btnTwit = document.getElementById("tweetMessage");
 
         //btnGenerate.addEventListener("click", downloadNow);    
 
